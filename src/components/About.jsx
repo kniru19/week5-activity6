76d49b09ae944a77b72d07f844f513f2838cdb7f
kniru19 <kniru19@gmail.com>
2025-02-19 01:40:32 +0545

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,31 @@
+import about from "../assets/images/about.jpeg";
+
+
 const About = () => {
   return (
     <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
+      <section class="section" id="about">
+          <div class="section-title">
+            <h2>about <span>us</span></h2>
+          </div>
+          <div class="section-center about-center">
+            <img src={about} alt="about" class="about-img" />
+            <article class="about-info">
+              <h3>explore the difference</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aspernatur quisquam harum nam cumque temporibus explicabo
+                dolorum sapiente odio unde dolor?
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aspernatur quisquam harum nam cumque temporibus explicabo
+                dolorum sapiente odio unde dolor?
+              </p>
+              <a href="#" class="btn">read more</a>
+            </article>
+          </div>
+        </section>
     </div>
   );
 };
